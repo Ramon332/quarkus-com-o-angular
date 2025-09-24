56646174cfbd40c8a841b67f43aa8d0c3cc764c9
RamonDuzanDev <271955@unifio.edu.br>
2025-09-24 23:35:45 +0000

    Adicionado CategoriaTests

--- quarkus-angular-projeto/src/main/java/entidades/Anexo.java
+++ quarkus-angular-projeto/src/main/java/entidades/Anexo.java
@@ -1,5 +1,7 @@
 package entidades;
 
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,7 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Anexo {
+public class Anexo extends PanacheEntityBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer codigo;
--- quarkus-angular-projeto/src/test/java/entidades/CategoriaTests.java
+++ quarkus-angular-projeto/src/test/java/entidades/CategoriaTests.java
@@ -1,8 +1,22 @@
 package entidades;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.transaction.Transactional;
 
 @QuarkusTest
 public class CategoriaTests {
+   @Test
+   @Transactional
+   public void testPersistirCategoria() {
+       Anexo anexo = new Anexo();
+       anexo.nomeArquivo = "vendas.csv";
+       anexo.persist();
 
+       Assertions.assertEquals(anexo.codigo, 8);
+   }
 }
+
